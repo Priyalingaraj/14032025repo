dbf682ec485a9cde4750e36e2a8b23d2864883be
Pooranipriya <pooranipriya.be@gmail.com>
2025-03-14 15:18:50 +0530

    finally added the Route table and Internet gateway with Association

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "my_ec2" {
   }
 }
 
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "my_bucket15032025" {
   bucket = "my-unique-bucket-name-12345"  # Make sure this bucket name is globally unique
   acl    = "private"  # You can choose other ACLs like "public-read" if needed
 
@@ -32,4 +32,29 @@ resource "aws_s3_bucket" "my_bucket" {
     Name        = "MyS3Bucket"
     Environment = "Development"
   }
+}
+
+
+# Create a Route Table
+resource "aws_route_table" "main_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"  # Allows outbound traffic to the internet
+    gateway_id = aws_internet_gateway.main_gateway.id
+  }
+
+  tags = {
+    Name = "MainRouteTable"
+  }
+}
+# Create an Internet Gateway
+resource "aws_internet_gateway" "main_gateway" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Associate the Route Table with the Subnet
+resource "aws_route_table_association" "main_association" {
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.main_route_table.id
 }
